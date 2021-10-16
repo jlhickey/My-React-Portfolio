a5ec3dead467c41e4ae46dbbf97c1e96438c79c9
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-16 12:28:02 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import InfoPanel from "./components/info-panel/InfoPanel";
+import DetailTabPanel from "./components/detail-tabpanel/DetailTabPanel";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <InfoPanel></InfoPanel>     
+        <DetailTabPanel></DetailTabPanel>
+      </div>
+    );
+  }
+}
+
+export default App;

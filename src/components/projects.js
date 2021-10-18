6cdb51e5b0df63bd7d03ecca79258a4cb772d8d4
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-18 12:06:08 -0400

    Create projects.js

---
+++ src/components/projects.js
@@ -0,0 +1,51 @@
+import pixaRadar from "../img/demo.jpeg"
+
+export const projects = 
+
+
+[
+    {
+        Name: "PixaRadar",
+        Description: "A program that will choose a movie for you based on the time of day, and your mood.",
+        ScreenShotURL: pixaRadar,
+        ProjectLink: "https://github.com/jlhickey/Project1",
+        GithubLink: "https://github.com/jlhickey"
+    },
+    {
+        Name: "Password Generator",
+        Description: "allow someone to get a new, and secure password every time.",
+        ProjectLink: "https://github.com/jlhickey/password-generator",
+        ScreenShotURL: pixaRadar,
+        GithubLink: "https://github.com/jlhickey"
+    },
+    {
+        Name: "Work Day Scheduler ",
+        Description: "A scheduler for people who need to keep their affairs in order.",
+        ScreenShotURL: pixaRadar,
+        ProjectLink: "https://github.com/jlhickey/Work-Day-Scheduler/",
+        GithubLink: "https://github.com/jlhickey/"
+    },
+    {
+        Name: "n-Abl",
+        Description: "An app that share mental health issue or challenge as a post so that people can obtain support from other members to help me cope with or overcome it..",
+        ScreenShotURL: pixaRadar,
+        ProjectLink: "https://github.com/askarrizvi/n-abl",
+        GithubLink: "https://github.com/jlhickey/"
+    },
+    {
+        Name: "MERN Book Search",
+        Description: "This app uses google to pull up any and all books that are input into the search bar.",
+        ScreenShotURL: pixaRadar,
+        ProjectLink: " https://get-it-done-budget.herokuapp.com/",
+        GithubLink: "https://github.com/jlhickey/"
+    },
+    {
+        Name: "Budget Tracker",
+        Description: "An app that allows track all expences.",
+        ScreenShotURL: pixaRadar,
+        ProjectLink: " https://get-it-done-budget.herokuapp.com/",
+        GithubLink: "https://github.com/jlhickey"
+    }
+]
+
+export default projects;

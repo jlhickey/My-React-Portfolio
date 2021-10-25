07b601de3f8dd5c695885ea549b8dc200e5abaed
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-24 23:16:14 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Accordion } from '@material-ui/core'import ReactDOM from "react-dom";
+import { Accordion } from '@material-ui/core';
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from ".src/App";
 import * as serviceWorker from "./serviceWorker";

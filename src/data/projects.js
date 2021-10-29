52cbb5a30420c8ac74e8602b93c0bed1cebdebca
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 18:53:08 -0400

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,14 +7,14 @@ export const projects =
      {
         Name: "Drinkstagram",
         Description: "This cocktail app will find whatever youare searching, and show you what is in it",
-        ScreenShotURL: pixaRadar,
+        // ScreenShotURL: pixaRadar,
         ProjectLink: " nameless-spire-26834.herokuapp.com/",
         GithubLink: "https://github.com/TylerLadas/Drinkstagram"         
      },
     {
         Name: "PixaRadar",
         Description: "A program that will choose a movie for you based on the time of day, and your mood.",
-        ScreenShotURL: pixaRadar,
+        // ScreenShotURL: pixaRadar,
         ProjectLink: "https://github.com/jlhickey/Project1",
         GithubLink: "https://github.com/jlhickey"
     },
@@ -22,34 +22,34 @@ export const projects =
         Name: "Password Generator",
         Description: "allow someone to get a new, and secure password every time.",
         ProjectLink: "https://github.com/jlhickey/password-generator",
-        ScreenShotURL: passGen,
+        // ScreenShotURL: passGen,
         GithubLink: "https://github.com/jlhickey"
     },
     {
         Name: "Work Day Scheduler ",
         Description: "A scheduler for people who need to keep their affairs in order.",
-        ScreenShotURL: myworkday,
+        // ScreenShotURL: myworkday,
         ProjectLink: "https://github.com/jlhickey/Work-Day-Scheduler/",
         GithubLink: "https://github.com/jlhickey/"
     },
     {
         Name: "n-Abl",
         Description: "An app that share mental health issue or challenge as a post so that people can obtain support from other members to help me cope with or overcome it..",
-        ScreenShotURL: nAbl,
+        // ScreenShotURL: nAbl,
         ProjectLink: "https://github.com/askarrizvi/n-abl",
         GithubLink: "https://github.com/jlhickey/"
     },
     {
         Name: "MERN Book Search",
         Description: "This app uses google to pull up any and all books that are input into the search bar.",
-        ScreenShotURL: book1,
+        // ScreenShotURL: book1,
         ProjectLink: " https://get-it-done-budget.herokuapp.com/",
         GithubLink: "https://github.com/jlhickey/"
     },
     {
         Name: "Budget Tracker",
         Description: "An app that allows track all expences.",
-        ScreenShotURL: screenshot,
+        // ScreenShotURL: screenshot,
         ProjectLink: " https://get-it-done-budget.herokuapp.com/",
         GithubLink: "https://github.com/jlhickey"
     }
